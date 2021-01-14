7ca4f6e27a3a50e10ef3a850ca58187b9a1d2004
cjrumble <cjrumble@users.noreply.github.com>
2021-01-13 22:04:54 -0800

    Initial commit

---
+++ src/IntEqualityPrinter.java
@@ -0,0 +1,18 @@
+/* ***************************************************************************
+This is a comment about the IntEqualityPrinter class/methods.
+*************************************************************************** */
+
+public class IntEqualityPrinter {
+
+    public static void printEqual(int one, int two, int three) {
+        if (one < 0 || two < 0 || three < 0) {
+            System.out.println("Invalid Value");
+        } else if (one == two && two == three){
+            System.out.println("All numbers are equal");
+        } else if (one != two && two != three && one != three) {
+            System.out.println("All numbers are different");
+        } else
+        System.out.println("Neither all are equal or different");
+    }
+
+}
